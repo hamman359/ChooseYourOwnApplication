02af69a1f6aaf8bb59c8dcc63c15b42b8e98951f
Jonathan Hammond <hamman359@gmail.com>
2014-02-01 20:52:54 -0500

    Adding User model.

---
+++ user.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+App.Models.User = Backbone.Model.extend({
+
+	logIn: function (userName) {
+		if (!userName) return;
+		this.set("userName", userName);
+	},
+
+	isLoggedIn: function () {
+
+		return this.has('userName');
+	}
+});
